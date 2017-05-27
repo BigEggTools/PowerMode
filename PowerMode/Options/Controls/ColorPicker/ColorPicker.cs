f9b2ba6ab2084c98f4c1d07ff8c457f09f7eb050
BigEgg <cd_bigegg@hotmail.com>
2017-05-27 15:36:45 +0800

    Add Color Picker control

---
+++ PowerMode/Options/Controls/ColorPicker/ColorPicker.cs
@@ -0,0 +1,189 @@
+﻿namespace BigEgg.Tools.PowerMode.Options.Controls
+{
+    using System.Drawing;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+
+    using BigEgg.Tools.PowerMode.Utils;
+
+    [TemplatePart(Name = PART_ColorPickerToggleButton, Type = typeof(ToggleButton))]
+    [TemplatePart(Name = PART_ColorPickerPalettePopup, Type = typeof(Popup))]
+    public class ColorPicker : Control
+    {
+        private const string PART_ColorPickerToggleButton = "PART_ColorPickerToggleButton";
+        private const string PART_ColorPickerPalettePopup = "PART_ColorPickerPalettePopup";
+
+        private ToggleButton toggleButton;
+        private Popup popup;
+        private Color originColor;
+
+
+        static ColorPicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorPicker), new FrameworkPropertyMetadata(typeof(ColorPicker)));
+        }
+
+
+        public ColorPicker()
+        {
+            Keyboard.AddKeyDownHandler(this, OnKeyDown);
+            Mouse.AddPreviewMouseDownOutsideCapturedElementHandler(this, OnMouseDownOutsideCapturedElement);
+        }
+
+
+        #region Dependency Properties
+        #region IsOpen
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register("IsOpen", typeof(bool), typeof(ColorPicker), new UIPropertyMetadata(false, OnIsOpenChanged));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        private static void OnIsOpenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ColorPicker colorPicker)
+            {
+                colorPicker.OnIsOpenChanged((bool)e.OldValue, (bool)e.NewValue);
+            }
+        }
+
+        private void OnIsOpenChanged(bool oldValue, bool newValue)
+        {
+            if (newValue) { originColor = SelectedColor; }
+            else { SelectedColor = originColor; }
+
+            RoutedEventArgs args = new RoutedEventArgs(newValue ? OpenedEvent : ClosedEvent, this);
+            RaiseEvent(args);
+        }
+        #endregion
+
+        #region SelectedColor
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color?), typeof(ColorPicker), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnSelectedColorPropertyChanged)));
+
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        private static void OnSelectedColorPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is ColorPicker colorPicker)
+            {
+                colorPicker.OnSelectedColorChanged((Color)e.OldValue, (Color)e.NewValue);
+            }
+        }
+
+        private void OnSelectedColorChanged(Color oldValue, Color newValue)
+        {
+            SelectedColorText = newValue.ToHexString();
+
+            var args = new RoutedPropertyChangedEventArgs<Color>(oldValue, newValue)
+            {
+                RoutedEvent = ColorPicker.SelectedColorChangedEvent
+            };
+            RaiseEvent(args);
+        }
+        #endregion
+
+        #region SelectedColorText
+        public static readonly DependencyProperty SelectedColorTextProperty = DependencyProperty.Register("SelectedColorText", typeof(string), typeof(ColorPicker), new UIPropertyMetadata(""));
+
+        public string SelectedColorText
+        {
+            get { return (string)GetValue(SelectedColorTextProperty); }
+            protected set { SetValue(SelectedColorTextProperty, value); }
+        }
+        #endregion
+        #endregion
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            toggleButton = this.Template.FindName(PART_ColorPickerToggleButton, this) as ToggleButton;
+            popup = GetTemplateChild(PART_ColorPickerPalettePopup) as Popup;
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            CloseColorPicker(true);
+        }
+
+        #region Control Events Handlers
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!IsOpen && e.Key == Key.Down)
+            {
+                OpenColorPick();
+                e.Handled = true;
+            }
+            if (IsOpen && e.Key == Key.Up)
+            {
+                CloseColorPicker(true);
+                e.Handled = true;
+            }
+            if (IsOpen && e.Key == Key.Escape)
+            {
+                CloseColorPicker(true);
+                e.Handled = true;
+            }
+        }
+
+        private void OnMouseDownOutsideCapturedElement(object sender, MouseButtonEventArgs e)
+        {
+            CloseColorPicker(true);
+        }
+        #endregion
+
+        #region Events
+        #region SelectedColorChangedEvent
+        public static readonly RoutedEvent SelectedColorChangedEvent = EventManager.RegisterRoutedEvent("SelectedColorChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<Color?>), typeof(ColorPicker));
+
+        public event RoutedPropertyChangedEventHandler<Color> SelectedColorChanged
+        {
+            add { AddHandler(SelectedColorChangedEvent, value); }
+            remove { RemoveHandler(SelectedColorChangedEvent, value); }
+        }
+        #endregion
+
+        #region OpenedEvent
+        public static readonly RoutedEvent OpenedEvent = EventManager.RegisterRoutedEvent("OpenedEvent", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ColorPicker));
+
+        public event RoutedEventHandler Opened
+        {
+            add { AddHandler(OpenedEvent, value); }
+            remove { RemoveHandler(OpenedEvent, value); }
+        }
+        #endregion
+
+        #region ClosedEvent
+        public static readonly RoutedEvent ClosedEvent = EventManager.RegisterRoutedEvent("ClosedEvent", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ColorPicker));
+
+        public event RoutedEventHandler Closed
+        {
+            add { AddHandler(ClosedEvent, value); }
+            remove { RemoveHandler(ClosedEvent, value); }
+        }
+        #endregion
+        #endregion
+
+        private void OpenColorPick()
+        {
+            IsOpen = true;
+        }
+
+        private void CloseColorPicker(bool isFocusOnColorPicker)
+        {
+            if (IsOpen) { IsOpen = false; }
+            ReleaseMouseCapture();
+            if (isFocusOnColorPicker && (toggleButton != null)) { toggleButton.Focus(); }
+        }
+    }
+}
---
+++ PowerMode/Options/Converters/InverseBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace BigEgg.Tools.PowerMode.Options.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
