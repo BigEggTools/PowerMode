5897a42cb22368da70d40aa3204ab7621385be52
BigEgg <cd_bigegg@hotmail.com>
2017-06-01 18:21:14 +0800

    Add output to show the progress of the export powershell script

--- gh-pages/export-gh-pages.ps1
+++ gh-pages/export-gh-pages.ps1
@@ -2,9 +2,14 @@ $docsFolderName = "..\docs";
 $angularOutputFolderName = "dist";
 
 If (Test-Path $docsFolderName) {
+    Write-Output "Cleanup the GitHub pages folder.";
     Remove-Item $docsFolderName -Recurse;
 }
 
+Write-Output "Start build the Angular SAP.";
 ng build --prod --base-href "https://bigeggtools.github.io/PowerMode/";
 
+Write-Output "Copy the Angular SAP to GitHub pages folder.";
 Copy-Item $angularOutputFolderName -Destination $docsFolderName -Recurse;
+
+Write-Output "Done.";
